f3f4e3f8e76328bb0dc523ac9f64d6ae4f866499
wu <jianhengwu0407@gmail.com>
2022-11-13 12:49:07 +0800

    添加数据库事务装饰器

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from tortoise import timezone
+from tortoise.transactions import atomic
 
 from backend.app.api import jwt
 from backend.app.crud.base import CRUDBase
@@ -15,12 +16,14 @@ class CRUDUser(CRUDBase[User, CreateUser, UpdateUser]):
     async def get_user_by_username(self, name: str) -> User:
         return await self.model.filter(username=name).first()
 
+    @atomic()
     async def update_user_login_time(self, pk: int) -> int:
         return await self.model.filter(id=pk).update(last_login=timezone.now())
 
     async def check_email(self, email: str) -> bool:
         return await self.model.filter(email=email).exists()
 
+    @atomic()
     async def register_user(self, user: CreateUser) -> User:
         user.password = jwt.get_hash_password(user.password)
         user = await self.create(user)
@@ -38,13 +41,16 @@ class CRUDUser(CRUDBase[User, CreateUser, UpdateUser]):
         user = await self.get_user_by_username(username)
         return user.avatar
 
+    @atomic()
     async def reset_password(self, username: str, password: str) -> int:
         new_pwd = jwt.get_hash_password(password)
         return await self.model.filter(username=username).update(password=new_pwd)
 
+    @atomic()
     async def update_userinfo(self, current_user: User, obj_in: UpdateUser) -> int:
         return await self.update(current_user.pk, obj_in)
 
+    @atomic()
     async def update_avatar(self, current_user: User, avatar: str):
         return await self.update(current_user.pk, {'avatar': avatar})
 
@@ -52,25 +58,29 @@ class CRUDUser(CRUDBase[User, CreateUser, UpdateUser]):
         user = await self.get(pk)
         return user.avatar
 
+    @atomic()
     async def delete_avatar(self, pk: int) -> int:
         return await self.model.filter(id=pk).update(avatar=None)
-
-    async def super_set(self, pk: int) -> int:
-        super_status = await self.get_user_super_status(pk)
-        return await self.model.filter(id=pk).update(is_superuser=False if super_status else True)
 
     async def get_user_super_status(self, pk: int) -> bool:
         user = await self.get(pk)
         return user.is_superuser
 
-    async def active_set(self, pk: int) -> int:
-        active_status = await self.get_user_active_status(pk)
-        return await self.model.filter(id=pk).update(is_active=False if active_status else True)
-
     async def get_user_active_status(self, pk: int) -> bool:
         user = await self.get(pk)
         return user.is_active
 
+    @atomic()
+    async def super_set(self, pk: int) -> int:
+        super_status = await self.get_user_super_status(pk)
+        return await self.model.filter(id=pk).update(is_superuser=False if super_status else True)
+
+    @atomic()
+    async def active_set(self, pk: int) -> int:
+        active_status = await self.get_user_active_status(pk)
+        return await self.model.filter(id=pk).update(is_active=False if active_status else True)
+
+    @atomic()
     async def delete_user(self, pk: int) -> int:
         return await self.delete(pk)
 
